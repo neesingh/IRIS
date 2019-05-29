fdb3f70814c8cd64fdbe983d173b268db33e56f4
eseemill <eseemill@indiana.edu>
2019-05-29 13:31:38 -0400

    Update message2matt.m

--- message2matt.m
+++ message2matt.m
@@ -3,3 +3,18 @@
 for i = 1:5
     fprintf('yay');
 end
+
+
+far_out = 1;
+this_is_neat = 0;
+
+while far_out == 1
+    this_is_neat = this_is_neat + 1;
+    
+    if mod(this_is_neat, 1000) == 0
+        far_out = 0;
+    end
+    
+end
+
+disp(this_is_neat);
